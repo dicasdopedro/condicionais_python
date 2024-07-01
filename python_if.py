da8fa94428fc83bbfa3d968c796c0e10345c935a
dicasdopedro <113735714+dicasdopedro@users.noreply.github.com>
2024-07-01 08:24:15 -0300

    Exemplos de Condicionais no Python
    If
    If Else
    If Elif Else

---
+++ python_if.py
@@ -0,0 +1,3 @@
+valor = 10
+if valor > 5:
+    print('O valor é maior que 5.')
---
+++ python_if_elif_else.py
@@ -0,0 +1,7 @@
+valor = 10
+if valor > 10:
+    print('O valor é maior que 10.')
+elif valor == 10:
+    print('O valor é igual a 10.')
+else:
+    print('O valor é menor que 10.')
